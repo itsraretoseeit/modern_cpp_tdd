2815ecc8d7b6e808daccf5312b8dc77110fb3d05
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-06-02 21:53:20 +0200

    limit max encoding length

--- chapter2/ex1/Soundex.hpp
+++ chapter2/ex1/Soundex.hpp
@@ -20,8 +20,15 @@ private:
 
     std::string encodedDigits(const std::string& word) const {
         std::string encoding;
-        for (auto letter : word) encoding += encodedDigit(letter);
+        for (auto letter : word) {
+            if (isComplete(encoding)) break;
+            encoding += encodedDigit(letter);
+        }
         return encoding;
+    }
+
+    bool isComplete(const std::string& encoding) const {
+        return encoding.length() == MaxCodeLength - 1;
     }
 
     std::string encodedDigit(char letter) const {
