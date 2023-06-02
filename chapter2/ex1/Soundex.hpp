742c718e54b28ee97b287f7540f2d09c45581315
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-06-02 22:05:20 +0200

    ignore duplicates

--- chapter2/ex1/Soundex.hpp
+++ chapter2/ex1/Soundex.hpp
@@ -7,6 +7,19 @@ public:
         return zeroPad(head(word) + encodedDigits(tail(word)));
     }
 
+    std::string encodedDigit(char letter) const {
+        const std::unordered_map<char, std::string> encodings {
+            {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+            {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+            {'d', "3"}, {'t', "3"},
+            {'l', "4"},
+            {'m', "5"}, {'n', "5"},
+            {'r', "6"}
+        };
+        auto it = encodings.find(letter);
+        return it == encodings.end() ? "" : it->second;
+    }
+
 private:
     static const size_t MaxCodeLength{4};
 
@@ -22,27 +35,22 @@ private:
         std::string encoding;
         for (auto letter : word) {
             if (isComplete(encoding)) break;
-            encoding += encodedDigit(letter);
+            if (encodedDigit(letter) != lastDigit(encoding))
+                encoding += encodedDigit(letter);
         }
         return encoding;
+    }
+
+    std::string lastDigit(const std::string& encoding) const {
+        if(encoding.empty()) return "";
+        return std::string(1, encoding.back());
     }
 
     bool isComplete(const std::string& encoding) const {
         return encoding.length() == MaxCodeLength - 1;
     }
 
-    std::string encodedDigit(char letter) const {
-        const std::unordered_map<char, std::string> encodings {
-            {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
-            {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
-            {'d', "3"}, {'t', "3"},
-            {'l', "4"},
-            {'m', "5"}, {'n', "5"},
-            {'r', "6"}
-        };
-        auto it = encodings.find(letter);
-        return it == encodings.end() ? "" : it->second;
-    }
+    
 
     std::string zeroPad(const std::string& word) const {
         auto zerosNeeded = MaxCodeLength - word.length();
--- chapter2/ex1/SoundexTest.cpp
+++ chapter2/ex1/SoundexTest.cpp
@@ -32,3 +32,15 @@ TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
 TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
     ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
 }
+
+TEST_F(SoundexEncoding, IgnoreVowelLikeLetters) {
+    ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
+    ASSERT_THAT(soundex.encodedDigit('b'), Eq(soundex.encodedDigit('f')));
+    ASSERT_THAT(soundex.encodedDigit('c'), Eq(soundex.encodedDigit('g')));
+    ASSERT_THAT(soundex.encodedDigit('d'), Eq(soundex.encodedDigit('t')));
+    ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
+
