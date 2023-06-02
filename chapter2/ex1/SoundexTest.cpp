772f1c43bc6d5b9f1d69466d959507ec612c44f2
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-06-02 20:58:32 +0200

    passing assertion, but stupid one

--- chapter2/ex1/SoundexTest.cpp
+++ chapter2/ex1/SoundexTest.cpp
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-class Soundex {};
+class Soundex {
+public:
+    std::string encode(const std::string& word) const {
+        return "A";
+    }
+};
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     Soundex soundex;
+
+    auto encoded = soundex.encode("A");
+
+    ASSERT_THAT(encoded, testing::Eq("A"));
 }
