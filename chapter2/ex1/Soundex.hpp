a5fd50c8bb518704da2e787e8f98695af4eb6def
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-06-03 17:51:49 +0200

    code refactor, follow SRP

--- chapter2/ex1/Soundex.hpp
+++ chapter2/ex1/Soundex.hpp
@@ -4,7 +4,7 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return zeroPad(upperFront(head(word)) + encodedDigits(tail(word)));
+        return zeroPad(upperFront(head(word)) + tail(encodedDigits(word)));
     }
 
     std::string encodedDigit(char letter) const {
@@ -42,13 +42,26 @@ private:
 
     std::string encodedDigits(const std::string& word) const {
         std::string encoding;
-        for (auto letter : word) {
-            if (isComplete(encoding)) break;
-            auto digit = encodedDigit(letter);
-            if (digit != NotADigit && digit != lastDigit(encoding))
-                encoding += digit;
-        }
+        encodeHead(encoding, word);
+        encodeTail(encoding, word);
         return encoding;
+    }
+
+    void encodeHead(std::string& encoding, const std::string& word) const {
+        encoding += encodedDigit(word.front());
+    }
+
+    void encodeTail(std::string& encoding, const std::string& word) const {
+        for (auto letter: tail(word)) {
+            if(!isComplete(encoding))
+                encodeLetter(encoding, letter);
+        }
+    }
+
+    void encodeLetter(std::string &encoding, char letter) const {
+        auto digit = encodedDigit(letter);
+        if (digit != NotADigit && digit != lastDigit(encoding))
+            encoding += digit;
     }
 
     std::string lastDigit(const std::string& encoding) const {
@@ -57,7 +70,7 @@ private:
     }
 
     bool isComplete(const std::string& encoding) const {
-        return encoding.length() == MaxCodeLength - 1;
+        return encoding.length() == MaxCodeLength;
     }
 
     std::string zeroPad(const std::string& word) const {
